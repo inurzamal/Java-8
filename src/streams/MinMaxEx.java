42861a80000ecef24381d52efcaec31f7af63353
Nurzamal Islam <n.zaman98@gmail.com>
2022-07-11 19:04:31 +0530

    updated

---
+++ src/streams/MinMaxEx.java
@@ -0,0 +1,18 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+
+public class MinMaxEx {
+
+	public static void main(String[] args) {
+		
+		List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29); //get this data from database	
+		IntSummaryStatistics statistics = primes.stream().mapToInt(x->x).summaryStatistics();	
+		System.out.println(statistics);
+		
+//		int statistics = primes.stream().mapToInt(x->x).summaryStatistics().getMax();	
+//		System.out.println(statistics);
+	}
+}
